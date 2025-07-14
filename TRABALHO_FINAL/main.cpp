937644266449b2546d56c9ac38a0c281a932fee5
VictorM-Coder <martinsvictor6781@gmail.com>
2025-07-14 10:02:19 -0300

    refac: finalizacao da main

--- TRABALHO_FINAL/main.cpp
+++ TRABALHO_FINAL/main.cpp
@@ -7,42 +7,39 @@
 #include "TestUtils.hpp"
 
 int main(int argc, char* argv[]) {
-    // if (argc != 4) {
-    //     std::cerr << "Uso: " << argv[0] << " <estrutura> <arquivo_entrada> <arquivo_saida>" << std::endl;
-    //     return 1;
-    // }
-    //
-    // std::string dataStruct = argv[1];
-    // std::string pathToRead = argv[2];
-    // std::string nameToWrite = argv[3];
-    //
-    // std::vector<std::string> all_words = FileUtils::readFile(pathToRead);
-    // std::cout << "size: " << all_words.size() << std::endl;
-    //
-    // Type chosenType;
-    // if (dataStruct == "dictionary_avl") {
-    //     chosenType = AVL_TREE;
-    // } else if (dataStruct == "dictionary_rb") {
-    //     chosenType = RB_TREE;
-    // } else if (dataStruct == "dictionary_cht") {
-    //     chosenType = CHAINED_HT;
-    // } else if (dataStruct == "dictionary_oht") {
-    //     chosenType = OPEN_HT;
-    // } else {
-    //     std::cerr << "Erro: estrutura de dados \"" << dataStruct << "\" não reconhecida." << std::endl;
-    //     return 1;
-    // }
-    //
-    // Dictionary dict(chosenType);
-    //
-    // for (const auto& word : all_words) {
-    //     dict.insert(word);
-    // }
-    //
-    // auto it = dict.getIterator();
-    // FileUtils::write_file(*it, nameToWrite);
+    if (argc != 4) {
+        std::cerr << "Uso: " << argv[0] << " <estrutura> <arquivo_entrada> <arquivo_saida>" << std::endl;
+        return 1;
+    }
 
-    TestUtils::runPipeline();
+    std::string dataStruct = argv[1];
+    std::string pathToRead = argv[2];
+    std::string nameToWrite = argv[3];
+
+    std::vector<std::string> all_words = FileUtils::readFile(pathToRead);
+
+    Type chosenType;
+    if (dataStruct == "dictionary_avl") {
+        chosenType = AVL_TREE;
+    } else if (dataStruct == "dictionary_rb") {
+        chosenType = RB_TREE;
+    } else if (dataStruct == "dictionary_cht") {
+        chosenType = CHAINED_HT;
+    } else if (dataStruct == "dictionary_oht") {
+        chosenType = OPEN_HT;
+    } else {
+        std::cerr << "Erro: estrutura de dados \"" << dataStruct << "\" não reconhecida." << std::endl;
+        return 1;
+    }
+
+    Dictionary dict(chosenType);
+
+    for (const auto& word : all_words) {
+        dict.insert(word);
+    }
+
+    auto it = dict.getIterator();
+    FileUtils::write_file(*it, nameToWrite);
 
     return 0;
 }
